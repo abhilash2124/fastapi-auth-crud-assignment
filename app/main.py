06081c84c0c02b576591c11f2a9ccb3bda443dc4
abhilash2124 <abhilashaddagatla86@gmail.com>
2026-03-26 20:06:03 +0530

    Enabled CORS for frontend-backend integration

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # allow all for now
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
